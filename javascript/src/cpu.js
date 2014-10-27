74b3419fe134c11209501bfbc39a6944a40d09b8
Tim Pickles <timpickles@gmail.com>
2014-10-27 12:24:21 +0000

    Implemented dojo 2

--- javascript/src/cpu.js
+++ javascript/src/cpu.js
@@ -9,7 +9,12 @@ function CPU(memorySize) {
 
     this.registers = {
         programCounter: 0,
-        a: 0
+        a: 0,
+        x: 0,
+        y: 0,
+        flags: {
+            equal: false
+        }
     };
 }
 
@@ -21,7 +26,7 @@ CPU.prototype.readMemory = function() {
     return this.memory[this.registers.programCounter];
 };
 
-CPU.prototype.writeMemory = function(value, memoryLocation) {
+CPU.prototype.writeMemory = function(memoryLocation, value) {
     return this.memory[memoryLocation] = value;
 };
 
@@ -46,6 +51,7 @@ CPU.prototype.execute = function() {
 
         switch(opCode) {
             case 0:
+                // BRK
                 running = false;
                 break;
             case 1:
@@ -61,7 +67,41 @@ CPU.prototype.execute = function() {
             case 3:
                 // STA
                 this.advanceProgramCounter();
-                this.writeMemory(this.registers.a, this.readMemory());
+                this.writeMemory(this.readMemory(), this.registers.a);
+                break;
+            case 4:
+                // LDX
+                this.advanceProgramCounter();
+                this.registers.x = this.readMemory();
+                break;
+            case 5:
+                // INX
+                this.registers.x++;
+                break;
+            case 6:
+                // CMY
+                this.advanceProgramCounter();
+                this.registers.flags.equal = (this.registers.y === this.readMemory());
+                break;
+            case 7:
+                // BNE
+                this.advanceProgramCounter();
+                if (!this.registers.flags.equal) {
+                    this.registers.programCounter = this.registers.programCounter + this.readMemory();
+                }
+                break;
+            case 8:
+                // STA_X
+                this.writeMemory(this.registers.x, this.registers.a);
+                break;
+            case 9:
+                // DEY
+                this.registers.y--;
+                break;
+            case 10:
+                // LDY
+                this.advanceProgramCounter();
+                this.registers.y = this.readMemory();
                 break;
             default:
                 throw new Error('Unknown operation');
--- javascript/test/specs/cpuSpec.js
+++ javascript/test/specs/cpuSpec.js
@@ -29,6 +29,18 @@ describe('CPU', function() {
         it('A register should start with value 0', function() {
             expect(cpu.registers.a).to.equal(0);
         });
+
+        it('X register should start with value 0', function() {
+            expect(cpu.registers.x).to.equal(0);
+        });
+
+        it('Y register should start with value 0', function() {
+            expect(cpu.registers.y).to.equal(0);
+        });
+
+        it('Flag equal should start with value false', function() {
+            expect(cpu.registers.flags.equal).to.be.false;
+        });
     });
 
     describe('Program Counter', function() {
@@ -72,6 +84,23 @@ describe('CPU', function() {
     });
 
     describe('Operations', function() {
+        it('should throw error when operation not recognised', function() {
+            var cpu = new CPU(5);
+
+            cpu.loadProgram([255]);
+
+            expect(cpu.execute.bind(cpu)).to.throw('Unknown operation');
+        });
+
+        it ('BRK: should increase the program counter by one and stop the CPU', function() {
+            var cpu = new CPU(5);
+
+            cpu.loadProgram([0]);
+            cpu.execute();
+
+            expect(cpu.registers.programCounter).to.equal(1);
+        });
+
         it('LDA: should load the A register with value supplied in the next memory address', function() {
             var cpu = new CPU(5);
 
@@ -103,12 +132,129 @@ describe('CPU', function() {
             expect(cpu.memory[5]).to.equal(107);
         });
 
-        it('should throw error when operation not recognised', function() {
+        it('STA_X: should store the value of the A register in the memory address referenced in the X register', function() {
+            var cpu = new CPU(6);
+
+            cpu.registers.a = 107;
+            cpu.registers.x = 5;
+
+            cpu.loadProgram([8]);
+            cpu.execute();
+
+            expect(cpu.memory[5]).to.equal(107);
+        });
+
+        it('LDX: should load value in the next memory address to the X register', function() {
+            var cpu = new CPU(3);
+
+            cpu.loadProgram([4, 99]);
+            cpu.execute();
+
+            expect(cpu.registers.x).to.equal(99);
+        });
+
+        it('INX: should increase the value in the X register by one', function() {
             var cpu = new CPU(5);
 
-            cpu.loadProgram([255]);
+            cpu.registers.x = 99;
 
-            expect(cpu.execute.bind(cpu)).to.throw('Unknown operation');
+            cpu.loadProgram([5]);
+            cpu.execute();
+
+            expect(cpu.registers.x).to.equal(100);
+        });
+
+        it('DEY: should decrease the value in the Y register by one', function() {
+            var cpu = new CPU(5);
+
+            cpu.registers.y = 101;
+
+            cpu.loadProgram([9]);
+            cpu.execute();
+
+            expect(cpu.registers.y).to.equal(100);
+        });
+
+        it('LDY: should load value in the next memory address to the Y register', function() {
+            var cpu = new CPU(3);
+
+            cpu.loadProgram([10, 123]);
+            cpu.execute();
+
+            expect(cpu.registers.y).to.equal(123);
+        });
+
+        it('CMY: should compare the value in register Y to the value in the next memory address and set the equal flag to true', function() {
+            var cpu = new CPU(3);
+
+            cpu.registers.y = 123;
+
+            cpu.loadProgram([6, 123]);
+            cpu.execute();
+
+            expect(cpu.registers.flags.equal).to.be.true;
+        });
+
+        it('CMY: should compare the value in register Y to the value in the next memory address and set the equal flag to false', function() {
+            var cpu = new CPU(3);
+
+            cpu.registers.y = 123;
+
+            cpu.loadProgram([6, 100]);
+            cpu.execute();
+
+            expect(cpu.registers.flags.equal).to.be.false;
+        });
+
+        it('BNE: should add the number in the next memory address to the program counter if the equal flag is set to false', function() {
+            var cpu = new CPU(10);
+
+            cpu.registers.programCounter = 3;
+            cpu.registers.flags.equal = false;
+
+            cpu.loadProgram([0, 0, 0, 7, -5]);
+            try {
+                cpu.execute();
+            } catch (e) {
+                expect(e.message).to.equal('BRK command encountered. Terminating CPU');
+            }
+
+            expect(cpu.registers.programCounter).to.equal(1); // because BRK increase the program counter by one
+        });
+
+        it('BNE: should not add the number in the next memory address to the program counter if the equal flag is set to true', function() {
+            var cpu = new CPU(10);
+
+            cpu.registers.programCounter = 5;
+            cpu.registers.flags.equal = true;
+
+            cpu.loadProgram([0, 0, 0, 7, -5, 0]);
+            cpu.execute();
+
+            expect(cpu.registers.programCounter).to.equal(6);
+        });
+    });
+
+    describe('Programs', function() {
+        it ('should write "who let the dogs out who who who " to the memory from address 128 onwards', function() {
+            var cpu = new CPU(256),
+                expectedMessage = 'who let the dogs out who who who ';
+
+            cpu.loadProgram([4, 128, 1, 0x77, 8, 5, 1, 0x68, 8, 5, 1, 0x6F, 8, 5, 1, 0x20, 8, 5, 1, 0x6c, 8, 5, 1, 0x65, 8, 5, 1, 0x74, 8, 5, 1, 0x20, 8, 5, 1, 0x74, 8, 5, 1, 0x68, 8, 5, 1, 0x65, 8, 5, 1, 0x20, 8, 5, 1, 0x64, 8, 5, 1, 0x6F, 8, 5, 1, 0x67, 8, 5, 1, 0x73, 8, 5, 1, 0x20, 8, 5, 1, 0x6F, 8, 5, 1, 0x75, 8, 5, 1, 0x74, 8, 5, 1, 0x20, 8, 5, 10, 3, 1, 0x77, 8, 5, 1, 0x68, 8, 5, 1, 0x6F, 8, 5, 1, 0x20, 8, 5, 9, 6, 0, 7, -21, 0]);
+            cpu.execute();
+
+            var message = '',
+                value;
+            for (var i = 128; i < 255; i++) {
+                value = cpu.memory[i];
+                if (value === 0) {
+                    break;
+                }
+
+                message += String.fromCharCode(value);
+            }
+
+            expect(message).to.equal(expectedMessage);
         });
     });
 });
